5f914f498acea50ebdba4fc000dd9d3bced5db00
abdellahbouziane <abouz77@gmail.com>
2023-01-20 19:48:47 -0500

    first page

--- src/Practice1/GitPractice.java
+++ src/Practice1/GitPractice.java
@@ -4,5 +4,7 @@ public class GitPractice {
     public static void main(String[] args) {
 
         System.out.println("Git is not easy to practice" );
+        System.out.println(" test start ");
+
     }
 }
