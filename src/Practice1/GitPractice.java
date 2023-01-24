3c2c96fd0b70303071a95eb0ae57bdb3c484ea6f
abdellahbouziane <abouz77@gmail.com>
2023-01-23 19:08:20 -0500

    first page updated

--- src/Practice1/GitPractice.java
+++ src/Practice1/GitPractice.java
@@ -3,7 +3,7 @@ package Practice1;
 public class GitPractice {
     public static void main(String[] args) {
 
-        System.out.println("Git is not easy to practice" );
+        System.out.println("Git  practice" );
         System.out.println(" test start ");
 
     }
